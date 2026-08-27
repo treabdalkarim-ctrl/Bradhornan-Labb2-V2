f7be70ccadd21143ba97cd4bcde104c46054a442
treabdalkarim-ctrl <treabdalkarim@gmail.com>
2026-08-27 13:10:33 +0200

    Add project files.

---
+++ labb2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace labb2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
